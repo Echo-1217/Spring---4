71ecb0451e9ac2ee1d8cafe3f744b18e657b516a
rorschach17 <s0958952199@gmail.com>
2022-10-28 15:42:59 +0800

    cmd

--- src/main/java/example/websocket/WebSocketApplication.java
+++ src/main/java/example/websocket/WebSocketApplication.java
@@ -1,12 +1,16 @@
 package example.websocket;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class WebSocketApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Runtime.getRuntime().exec("cmd.exe /c  docker-compose -f broker-compose.yml up mq" );
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
 
